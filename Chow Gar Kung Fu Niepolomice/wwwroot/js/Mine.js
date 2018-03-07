ae4fe88b0ca5c04789f81d7042e3cbeb143e01a1
Marek Kotowski <marek.kotowski87@gmail.com>
2018-03-07 23:39:09 +0100

    -breakpoint for changing font size in main menu items -color of collapsed menu button changed to red

--- Chow Gar Kung Fu Niepolomice/wwwroot/js/Mine.js
+++ Chow Gar Kung Fu Niepolomice/wwwroot/js/Mine.js
@@ -1,5 +1,15 @@
-﻿$('ul.nav li.dropdown').hover(function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-}, function () {
+﻿$(document).ready(function () {
+
+    $('ul.nav li.dropdown').hover(function () {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);}, function () {
     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
+
+    $(window).on('resize', function () {
+        if (($(window).width() >= 991) && ($(window).width() <= 1199)) {
+            $(".my-menu-item").css("cssText", "font-size: 17px !important;");
+        } else {
+            $(".my-menu-item").css("cssText", "font-size: 22px !important;");
+        }
+});
 });
